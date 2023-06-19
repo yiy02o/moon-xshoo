29b61120c06ff08451063188729aac47a6fde57d
yiy02o <rodrigo.albornoz.m@ing.uchile.cl>
2023-06-19 13:03:55 -0400

    Agrega la opción de normalizar cada espectro individual en la función median_2DSLIT().

--- 1D_updatefits.py
+++ 1D_updatefits.py
@@ -1,1 +1,66 @@
 from moon_preMolecfit import *
+
+
+def copy_and_update_fits(directory, ARM):
+    for folder in os.listdir(directory):
+        if folder == 'README':
+            pass
+        else:
+            for file in os.listdir(directory + '/' + folder):
+                if 'SCI_SLIT_FLUX_MERGE1D_' + ARM in file:
+                    # with fits.open(directory + '/' + folder + '/' + file) as hdu_list:
+                    print('aqui')
+                    # hdu_list.writeto('updated_' + file)
+
+            pass
+    pass
+
+
+share_path = '/home/yiyo/reflexData_mapMode_Highlands/reflex_end_products/2023-05-28T23:33:43/' \
+             'XSHOO.2018-03-10T08:00:28.424_tpl/'
+path_old_1D = share_path + 'MOV_Luna_highlands_2_SCI_SLIT_FLUX_MERGE1D_VIS.fits'
+path_new_1D = share_path + 'MOV_Luna_highlands_2_SCI_SLIT_FLUX_MERGE1D_VIS_UPDATE.fits'
+path_2D = share_path + 'MOV_Luna_highlands_2_SCI_SLIT_FLUX_MERGE2D_VIS.fits'
+
+hdul_old = fits.open(path_old_1D)
+hdul_new = fits.open(path_new_1D)
+hdul_2D = fits.open(path_2D)
+
+median_2D = np.median(hdul_2D[0].data, axis=0)
+std_2D = np.std(hdul_2D[0].data, axis=0)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+fig.suptitle('Antes del cambio', fontsize=24)
+
+axs[0].plot(hdul_old[0].data[1000:], label='Old 1D')
+axs[0].plot(hdul_new[0].data[1000:], label='New 1D')
+axs[0].legend()
+axs[0].grid()
+
+axs[1].plot(median_2D[1000:], label='Median 2D')
+axs[1].plot(hdul_new[0].data[1000:], label='New 1D')
+axs[1].legend()
+axs[1].grid()
+
+plt.show()
+plt.clf()
+
+
+with fits.open(path_new_1D, mode='update') as fits_file:
+    fits_file[0].data = median_2D
+    fits_file[1].data = std_2D
+
+fig2, axs2 = plt.subplots(1, 2, figsize=(10, 5))
+fig2.suptitle('Después del cambio', fontsize=24)
+
+axs2[0].plot(hdul_old[0].data[1000:], label='Old 1D')
+axs2[0].plot(hdul_new[0].data[1000:], label='New 1D')
+axs2[0].legend()
+axs2[0].grid()
+
+axs2[1].plot(median_2D[1000:], label='Median 2D')
+axs2[1].plot(hdul_new[0].data[1000:], label='New 1D')
+axs2[1].legend()
+axs2[1].grid()
+
+plt.show()
--- moon_preMolecfit.py
+++ moon_preMolecfit.py
@@ -29,7 +29,6 @@ def plot_default(directory, plot=False, verbose=False):
                 axs.set_title(obj_region + ' at ' + fq_range + ' range')
                 axs.set_xlabel('Wavelenght [' + quantts_col['WAVE'].unit + ']')
                 axs.set_ylabel('F [' + quantts_col['FLUX_REDUCED'].unit + ']')
-                #axs.set_ylim(top=150_000)
                 axs.grid()
                 plt.show()
                 break
@@ -37,7 +36,7 @@ def plot_default(directory, plot=False, verbose=False):
                 return bin_table
 
 
-def median_2DSLIT(directory, ARM, plot=False, axes=None):
+def median_2DSLIT(directory, ARM, normalize=False, plot=False, axes=None):
     """Calcula al mediana a lo largo del ancho de una slit en función de la frecuencia. Es una cantidad
     por pointing."""
     for folder in os.listdir(directory):
@@ -60,77 +59,80 @@ def median_2DSLIT(directory, ARM, plot=False, axes=None):
                     bin_table = hdul[1].data[0]
                     obj_region = hdul[0].header['HIERARCH ESO OBS NAME']
                     quants_col = hdul[1].columns
-    if plot:
-        wv = bin_table['WAVE']
-        axes.plot(wv[700:], median_flux[700:], lw=0.5, label=date_obs)
-        axes.set_xlabel('[' + quants_col['WAVE'].unit + ']', fontsize=11)
-        axes.set_ylabel('erg ' + r'$cm^{-2} s^{-1} \AA^{-1}$', fontsize=11)
-        # axes.plot(wv, flux2D, lw=0.5, label=date_obs)
-        axes.grid()
-        axes.legend(fontsize=13)
+    idx_avoid = 700
+    # normalize range
+    if ARM == 'NIR':
+        idx_min, idx_max = -21_000, -19_000
+    if ARM == 'VIS':
+        idx_min, idx_max = -10_000, -8_000
+    if ARM == 'UVB':
+        idx_min, idx_max = -5_000, -3_000
+    if normalize:
+        flux_test = median_flux[idx_min:idx_max + 1]
+        median_test_flux = np.median(flux_test)
+        flux_norm = median_flux/median_test_flux
+        std_norm = std_flux/median_test_flux
+        if plot:
+            wv = bin_table['WAVE']
+            w_min, w_max = wv[idx_min], wv[idx_max]
+            axes.plot(wv[idx_avoid:], flux_norm[idx_avoid:], lw=0.5, label=date_obs, color='b')
+            axes.axvspan(w_min, w_max, alpha=0.1, color='k')
+            #axes.fill_between(wv[idx_avoid:], flux_norm[idx_avoid:] - std_norm[idx_avoid:],
+                              #flux_norm[idx_avoid:] + std_norm[idx_avoid:], alpha=0.3, color='k')
+            axes.set_xlabel('[' + quants_col['WAVE'].unit + ']', fontsize=11)
+            axes.set_ylabel('Normalized flux', fontsize=11)
+            axes.grid()
+            axes.legend(fontsize=13)
+        else:
+            return flux_norm, bin_table, obj_region, quants_col
 
     else:
-        return median_flux, bin_table, obj_region, quants_col
+        if plot:
+            wv = bin_table['WAVE']
+            axes.plot(wv[idx_avoid:], median_flux[idx_avoid:], lw=0.5, label=date_obs)
+            axes.set_xlabel('[' + quants_col['WAVE'].unit + ']', fontsize=11)
+            axes.set_ylabel('erg ' + r'$cm^{-2} s^{-1} \AA^{-1}$', fontsize=11)
+            axes.grid()
+            axes.legend(fontsize=13)
+        else:
+            return median_flux, bin_table, obj_region, quants_col
 
 
-def medianALL_per_Region(directory, ARM, axes, color='b', normalized=False, plot=True):
+def medianALL_per_Region(directory, ARM, axes, color='b', normalize=False, plot=True):
     """Plotea y calcula la mediana de todos los espectros en las 3 bandas, que se tomaron en cierta zona del objeto.
     Es una cantidad por región del objeto."""
     fluxes = []
     wvs_list = []
     for run_folder in os.listdir(directory):
-        flux, bin_table, loc, col = median_2DSLIT(directory + run_folder, ARM)
+        flux, bin_table, loc, col = median_2DSLIT(directory + run_folder, ARM, normalize=normalize)
         fluxes.append(flux)
         wvs_list.append(bin_table['WAVE'])
         region = loc
         quants_columns = col
     if len(fluxes) == 19:   # We discard the last bad data of highlands pointing
         fluxes = fluxes[:-1]
+    if len(fluxes) == 16 and ARM == 'NIR':
+        fluxes = fluxes[:-2]
     print(len(fluxes))
     median_ALL = np.median(np.asarray(fluxes), axis=0)
     std_ALL = np.std(np.asarray(fluxes), axis=0)
     idx_avoid = 700
-    idx_min, idx_max = -20_000, -19_000  # normalize range
-    if ARM == 'UVB':
-        idx_min, idx_max = -12_000, -11_000
-    w_min, w_max = wvs_list[0][idx_min], wvs_list[0][idx_max]
-    if normalized:
-        flux_test = median_ALL[idx_min:idx_max + 1]
-        median_test_flux = np.median(flux_test)
-        flux_norm = median_ALL/median_test_flux
-        std_norm = std_ALL/median_test_flux
-        if plot:
-            axes.plot(wvs_list[0][idx_avoid:], flux_norm[idx_avoid:], linewidth=0.5, color=color, label=region)
-            # axes.fill_between(wvs_list[0][idx_avoid:], flux_norm[idx_avoid:] - std_norm[idx_avoid:],
-                            # flux_norm[idx_avoid:] + std_norm[idx_avoid:], alpha=0.3, color=color)
-            axes.axvline(w_min, color='b', alpha=0.1)
-            axes.axvline(w_max, color='b', alpha=0.1)
-            axes.axvspan(w_min, w_max, alpha=0.1, color='b')
-            axes.axhline(1., color='grey', ls='--', lw=3, alpha=0.8)
-            axes.set_ylabel('Flux normalized', fontsize=13)
-            axes.set_xlabel('[' + quants_columns['WAVE'].unit + ']', fontsize=13)
-            axes.set_title(ARM, fontsize=16)  # --> to plot per arm joint regions
-            axes.tick_params(axis='both', labelsize=12)
-            axes.legend(fontsize=13)
-        else:
-            return np.array([np.asarray(wvs_list[0][idx_avoid:]), flux_norm[idx_avoid:], std_norm[idx_avoid:]])
-
+    if plot:
+        axes.plot(wvs_list[0][idx_avoid:], median_ALL[idx_avoid:], linewidth=0.5, color=color, label=region)
+        axes.axhline(1., color='grey', ls='--', lw=3, alpha=0.8)
+        y_label = r'$I$' + quants_columns['FLUX'].unit + ']'
+        if normalize:
+            y_label = 'Flux normalized'
+        axes.set_ylabel(y_label, fontsize=13)
+        axes.set_xlabel('[' + quants_columns['WAVE'].unit + ']', fontsize=13)
+        axes.set_title(ARM, fontsize=16)  # --> to plot per arm joint regions
+        axes.tick_params(axis='both', labelsize=12)
+        axes.legend(fontsize=13)
     else:
-        if plot:
-            # from idx_avoid = [:700] to avoid spectrograph noise (?)
-            axes.plot(wvs_list[0][idx_avoid:], median_ALL[idx_avoid:], linewidth=0.5, color=color, label=region)
-            axes.fill_between(wvs_list[0][idx_avoid:], median_ALL[idx_avoid:] - std_ALL[idx_avoid:],
-                              median_ALL[idx_avoid:] + std_ALL[idx_avoid:], alpha=0.3)
-            axes.set_ylabel(r'$I$' + quants_columns['FLUX'].unit + ']', fontsize=13)
-            axes.set_xlabel('[' + quants_columns['WAVE'].unit + ']', fontsize=13)
-            axes.set_title(ARM, fontsize=16)  # --> to plot per arm joint regions
-            axes.tick_params(axis='both', labelsize=12)
-            axes.legend(fontsize=13)
-        else:
-            return np.array([np.asarray(wvs_list[0][idx_avoid:]), median_ALL[idx_avoid:], std_ALL[idx_avoid:]])
+        return np.array([np.asarray(wvs_list[0][idx_avoid:]), median_ALL[idx_avoid:], std_ALL[idx_avoid:]])
 
 
-def plot_medianZone_together(path_arr, normalized=False, plot=True):
+def plot_medianZone_together(path_arr, normalize=False, plot=True):
     """ Plotea las mediana del espectro por zona (tres espectros promedios -> HL, Maria y Darkside)
     """
     ARMS = ['UVB', 'VIS', 'NIR']
@@ -138,38 +140,44 @@ def plot_medianZone_together(path_arr, normalized=False, plot=True):
         fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(15, 10), layout="constrained")
         for arm, ax in zip(ARMS, axs):
             ax.grid()
-            for c, path in zip(['k', 'g', 'g'], path_arr):
-                medianALL_per_Region(path, arm, ax, color=c, normalized=normalized, plot=plot)
+            for c, path in zip(['b', 'r', 'k'], path_arr):
+                medianALL_per_Region(path, arm, ax, color=c, normalize=normalize, plot=plot)
         plt.show()
     else:
         ax = None
         per_band = []
         wave_per_band = []
         for idx, arm in enumerate(ARMS):
-            per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalized=normalized, plot=plot)[1] for path
+            per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalize=normalize, plot=plot)[1] for path
                                      in path_arr])
-            wave_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalized=normalized, plot=plot)[0] for
+            wave_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalize=normalize, plot=plot)[0] for
                                           path in path_arr])
             per_band.append(per_region)
             wave_per_band.append(wave_per_region[0])
         return wave_per_band, per_band
 
 
-def residues(path_arr, plot=False):
+def residues(path_arr, plot=False, norm_with_res=False):
     """Calcula los resiuduos de flujos normalizados entre dos zonas"""
     wave_per_band = []
+    F0 = []
+    F1 = []
     res_per_band = []
     std_per_band = []
     ax = None
     ARMS = ['UVB', 'VIS', 'NIR']
     for arm in ARMS:
-        std_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalized=True, plot=False)[2] for path
+        std_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalize=True, plot=False)[2] for path
                                      in path_arr])
-        flux_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalized=True, plot=False)[1] for path
+        flux_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalize=True, plot=False)[1] for path
                                       in path_arr])
-        wave_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalized=True, plot=False)[0] for path
+        wave_per_region = np.asarray([medianALL_per_Region(path, arm, ax, normalize=True, plot=False)[0] for path
                                       in path_arr])
-        res = flux_per_region[0] - flux_per_region[1]
+        flux_0 = flux_per_region[0]
+        F0.append(flux_0)
+        flux_1 = flux_per_region[1]
+        F1.append(flux_1)
+        res = flux_0 - flux_1
         res_per_band.append(res)
         wave_per_band.append(wave_per_region[0])
         std_per_band.append(std_per_region)
@@ -178,13 +186,16 @@ def residues(path_arr, plot=False):
         zone_0 = path_arr[0].partition('_')[-1].partition('_')[-1].partition('/')[0]
         zone_1 = path_arr[1].partition('_')[-1].partition('_')[-1].partition('/')[0]
         fig.suptitle(zone_0 + ' - ' + zone_1, fontsize=35)
-        for w, res, std, arm, ax in zip(wave_per_band, res_per_band, std_per_band, ARMS, axs):
-            ax.plot(w, res, lw=0.5)
-            #ax.fill_between(w, -np.sqrt((std[0]**2+std[1]**2)), np.sqrt((std[0]**2+std[1]**2)), alpha=0.1, color='k')
-            ax.fill_between(w, -std[1], std[1], alpha=0.3, color='r')
+        for w, res, f0, f1, std, arm, ax in zip(wave_per_band, res_per_band, F0, F1, std_per_band, ARMS, axs):
+            ax.plot(w, res, lw=0.3, color='k', alpha=1)
+            if norm_with_res:
+                ax.plot(w, f0, lw=0.3, color='b', label=zone_0)
+                ax.plot(w, f1, lw=0.3, color='orange', label=zone_1)
+                ax.legend()
+            sigma_i = np.sqrt((std[0]**2+std[1]**2))
+            ax.fill_between(w, -sigma_i, sigma_i, alpha=0.1, color='k')
             ax.set_xlabel('[nm]', fontsize=13)
             ax.set_ylabel('Res', fontsize=13)
-            #ax.axhline(0, ls='--', lw=2, color='k')
             ax.grid()
             ax.set_title(arm, fontsize=13)
             ax.tick_params(axis='both', labelsize=12)
@@ -207,16 +218,17 @@ path_dark_side = '/home/yiyo/reflexData_mapMode_Darkside/reflex_end_products/'
 
 
 path_arrays = [path_Highlands, path_Maria, path_dark_side]
-"""
+
 n_subdir = len(os.listdir(path_Highlands))
+
 fig, axs = plt.subplots(2, 2, figsize=(30, 15), layout="constrained")
 path_0 = path_dark_side
-arm_0 = 'VIS'
+arm_0 = 'NIR'
 for f, ax in zip(os.listdir(path_0), fig.axes):
-    median_2DSLIT(path_0 + f, arm_0, plot=True, axes=ax)
-fig.suptitle(path_0.partition('_')[-1].partition('/')[0].partition('_')[-1] + ' {}'.format(arm_0), fontsize=15)
+    median_2DSLIT(path_0 + f, arm_0, normalize=True, plot=True, axes=ax)
+    fig.suptitle(path_0.partition('_')[-1].partition('/')[0].partition('_')[-1] + ' {}'.format(arm_0), fontsize=15)
 plt.show()
-"""
+
 
 '''
 # Plotea las mediana del espectro por zona (tres espectros promedios -> HL, Maria y Darkside)
